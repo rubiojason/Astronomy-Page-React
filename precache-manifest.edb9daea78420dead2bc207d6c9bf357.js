46e20fe685308d0ddb8b1a0470b1f3a1ce59f353
Jason Rubio <jason@Denises-iMac.local>
2020-10-02 20:43:50 -0700

    Updates

---
+++ precache-manifest.edb9daea78420dead2bc207d6c9bf357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4711d881d5ca019bec6be2936951e177",
+    "url": "/Astronomy-Page-React/index.html"
+  },
+  {
+    "revision": "50a3bf122ce309179f34",
+    "url": "/Astronomy-Page-React/static/css/main.755d4339.chunk.css"
+  },
+  {
+    "revision": "a98bb460b96eeecaa8a9",
+    "url": "/Astronomy-Page-React/static/js/2.06d3fdd5.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/Astronomy-Page-React/static/js/2.06d3fdd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50a3bf122ce309179f34",
+    "url": "/Astronomy-Page-React/static/js/main.646a855a.chunk.js"
+  },
+  {
+    "revision": "f11bcb8d2f4a37d77c0f",
+    "url": "/Astronomy-Page-React/static/js/runtime-main.3d14fd9b.js"
+  }
+]);
